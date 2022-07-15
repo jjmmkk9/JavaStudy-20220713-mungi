82ed94a87001c4644af4dbd81cddae53d6baff8d
jjmmkk9 <abcdqwerty0192@gmail.com>
2022-07-15 11:06:12 +0900

    챕터1 출력

---
+++ src/j01_출력/PrintTest.java
@@ -0,0 +1,13 @@
+package j01_출력;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java!\n");   /*  "\n" 은 문자열이 아닌 명령으로 인식 =  줄바꿈 */
+		System.out.print("조문기");
+		
+		
+
+	}
+
+}
